189261757bea3438e501fffbb6bef60b7cf1feb9
Rodrigo-Kelven <kelvenr40@gmail.com>
2026-02-18 09:42:42 -0300

    update in services

--- serviceb/src/main/java/com/example/serviceb/controller/ServiceBRestController.java
+++ serviceb/src/main/java/com/example/serviceb/controller/ServiceBRestController.java
@@ -1,10 +1,12 @@
 package com.example.serviceb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ServiceBRestController {
