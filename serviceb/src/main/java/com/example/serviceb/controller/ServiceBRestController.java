4dd4842914504cfbdbd16f65066a78a7f97442bf
Rodrigo-Kelven <kelvenr40@gmail.com>
2026-02-18 17:46:43 -0300

    add spring cloud gateway

--- serviceb/src/main/java/com/example/serviceb/controller/ServiceBRestController.java
+++ serviceb/src/main/java/com/example/serviceb/controller/ServiceBRestController.java
@@ -1,30 +1,17 @@
 package com.example.serviceb.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
+
 
 @RestController
+@RequestMapping("/service-b")
 public class ServiceBRestController {
 
-    private final DiscoveryClient discoveryClient;
-    private final RestClient restClient;
-
-    public ServiceBRestController(DiscoveryClient discoveryClient, RestClient.Builder restClientBuilder) {
-        this.discoveryClient = discoveryClient;
-        restClient = restClientBuilder.build();
-    }
-
-    @GetMapping("helloEureka")
+    @GetMapping("/helloWorld")
     public String helloWorld() {
-        ServiceInstance serviceInstance = discoveryClient.getInstances("servicea").get(0);
-        return restClient.get()
-                .uri(serviceInstance.getUri() + "/helloWorld")
-                .retrieve()
-                .body(String.class);
+        return "Hello world from Service B!";
     }
 }
